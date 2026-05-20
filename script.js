cd6619347cc91cb7ea8d4da93e20638ee5718727
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-05-20 23:09:34 +0800

    mencoba js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+// 1. Tentukan nominal awal pemasukan
+let totalPemasukan = 0;
 
-function NambahUang() {
-    let uang_masuk = document.getElementById("uang-masuk");
-}
+// 2. Ambil elemen teks RpXXX tersebut berdasarkan ID-nya
+const tombolPemasukan = document.getElementById('uangmasuk');
+
+// 3. Jalankan fungsi ini setiap kali teks tersebut diklik
+tombolPemasukan.addEventListener('click', function() {
+    // Tambah nilai total sebanyak 1000
+    totalPemasukan += 1000;
+    
+    // Update teks di HTML dengan format Rupiah (.toLocaleString agar ada titik ribuan)
+    tombolPemasukan.innerText = "Rp" + totalPemasukan.toLocaleString('id-ID') + ",-";
+});
+
+let totalPengeluaran = 0;
+const tombolPengeluaran = document.getElementById('uangkeluar');
+
+tombolPengeluaran.addEventListener('click', function() {
+    totalPengeluaran += 500;
+    tombolPengeluaran.innerText = "Rp" + totalPengeluaran.toLocaleString('id-ID') + ",-";
+});
