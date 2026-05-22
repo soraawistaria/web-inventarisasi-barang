408deb5dc625c09b2887d3df695eefee641114f1
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-05-22 15:33:04 +0800

    banyak

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
+<?php 
+// 1. Hubungkan dengan file koneksi
+include 'koneksi.php'; 
+
+// 2. Ambil semua data dari tabel produk
+$ambil_data = mysqli_query($koneksi, "SELECT * FROM produk");
+?>
+
 <!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <link rel="stylesheet" href="style.css?v=1">
     <title align="center">Website Warung Permai</title>
 </head>
 <body>
@@ -11,7 +20,7 @@
         <h1>WARUNG PERMAI</h1>
         <div class="bagian-atas">
             <div class="pem-peng">
-                <p>Pemasukan</p><br>
+                <p>Pemasukan</p>
                 <p id="uangmasuk">Rp0,-</p>
             </div>
             <div id="rekap">
@@ -20,7 +29,7 @@
                 </a>
             </div>
             <div class="pem-peng">
-                <p>Pengeluaran</p><br>
+                <p>Pengeluaran</p>
                 <p id="uangkeluar">Rp0,-</p>
             </div>
         </div>
@@ -44,7 +53,7 @@
                 Cari produk : <input type="search" placeholder="Mie">
             </div>
             <div id="tmbhbrg">
-                <a href="tambahkanbarang.html">Tambahkan barang</a>
+                <a href="tambahkanbarang.php">Tambahkan barang</a>
             </div>
         </div>
     </header>
@@ -53,17 +62,23 @@
 
         <!-- Tampilan Katalog -->
     <main class="katalogs">
-        <div class="katalog">
-            <a href="produk\indomie.html">
-                <img src="gambar\indomie.jpg"><br>
-                Mie Indomie</a>
-        </div>
-        <div class="katalog">
-            <a href="produk\ultramilk.html">
-                <img src="gambar\ultramilk.jpg"><br>
-                Ultramilk all variant</a>
-        </div>
+        <?php 
+        // Perulangan akan berjalan otomatis sebanyak jumlah baris data di database
+        while($row = mysqli_fetch_assoc($ambil_data)) { 
+        ?>
+            
+            <div class="katalog">
+                <a href="produk.php?id=<?php echo $row['id']; ?>">
+                    <img src="gambar/<?php echo $row['gambar']; ?>" alt=""><br>
+                    <?php echo $row['nama_produk']; ?>
+                </a>
+                <p>Rp <?php echo number_format($row['harga']); ?>,-</p>
+                <p>Stok: <?php echo $row['stok']; ?></p>
+            </div>
 
+        <?php 
+        } 
+        ?>
     </main>
     <footer class="footer">
         <div id="footer">
--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-// 1. Tentukan nominal awal pemasukan
-let totalPemasukan = 0;
+// // 1. Tentukan nominal awal pemasukan
+// let totalPemasukan = 0;
 
-// 2. Ambil elemen teks RpXXX tersebut berdasarkan ID-nya
-const tombolPemasukan = document.getElementById('uangmasuk');
+// // 2. Ambil elemen teks RpXXX tersebut berdasarkan ID-nya
+// const tombolPemasukan = document.getElementById('uangmasuk');
 
-// 3. Jalankan fungsi ini setiap kali teks tersebut diklik
-tombolPemasukan.addEventListener('click', function() {
-    // Tambah nilai total sebanyak 1000
-    totalPemasukan += 1000;
+// // 3. Jalankan fungsi ini setiap kali teks tersebut diklik
+// tombolPemasukan.addEventListener('click', function() {
+//     // Tambah nilai total sebanyak 1000
+//     totalPemasukan += 1000;
     
-    // Update teks di HTML dengan format Rupiah (.toLocaleString agar ada titik ribuan)
-    tombolPemasukan.innerText = "Rp" + totalPemasukan.toLocaleString('id-ID') + ",-";
-});
+//     // Update teks di HTML dengan format Rupiah (.toLocaleString agar ada titik ribuan)
+//     tombolPemasukan.innerText = "Rp" + totalPemasukan.toLocaleString('id-ID') + ",-";
+// });
 
-let totalPengeluaran = 0;
-const tombolPengeluaran = document.getElementById('uangkeluar');
+// let totalPengeluaran = 0;
+// const tombolPengeluaran = document.getElementById('uangkeluar');
 
-tombolPengeluaran.addEventListener('click', function() {
-    totalPengeluaran += 500;
-    tombolPengeluaran.innerText = "Rp" + totalPengeluaran.toLocaleString('id-ID') + ",-";
-});
+// tombolPengeluaran.addEventListener('click', function() {
+//     totalPengeluaran += 500;
+//     tombolPengeluaran.innerText = "Rp" + totalPengeluaran.toLocaleString('id-ID') + ",-";
+// });
