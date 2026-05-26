6206e1c7f044919f38ba5044539d19dd951bf2b9
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-05-26 13:42:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-// // 1. Tentukan nominal awal pemasukan
-// let totalPemasukan = 0;
+let totalPemasukan = 0;
 
-// // 2. Ambil elemen teks RpXXX tersebut berdasarkan ID-nya
-// const tombolPemasukan = document.getElementById('uangmasuk');
+const tombolPemasukan = document.getElementById('uangmasuk');
 
-// // 3. Jalankan fungsi ini setiap kali teks tersebut diklik
-// tombolPemasukan.addEventListener('click', function() {
-//     // Tambah nilai total sebanyak 1000
-//     totalPemasukan += 1000;
+tombolPemasukan.addEventListener('click', function() {
+    totalPemasukan += 1000;
     
-//     // Update teks di HTML dengan format Rupiah (.toLocaleString agar ada titik ribuan)
-//     tombolPemasukan.innerText = "Rp" + totalPemasukan.toLocaleString('id-ID') + ",-";
-// });
+    tombolPemasukan.innerText = "Rp" + totalPemasukan.toLocaleString('id-ID') + ",-";
+});
 
-// let totalPengeluaran = 0;
-// const tombolPengeluaran = document.getElementById('uangkeluar');
+let totalPengeluaran = 0;
+const tombolPengeluaran = document.getElementById('uangkeluar');
 
-// tombolPengeluaran.addEventListener('click', function() {
-//     totalPengeluaran += 500;
-//     tombolPengeluaran.innerText = "Rp" + totalPengeluaran.toLocaleString('id-ID') + ",-";
-// });
+tombolPengeluaran.addEventListener('click', function() {
+    totalPengeluaran += 500;
+    tombolPengeluaran.innerText = "Rp" + totalPengeluaran.toLocaleString('id-ID') + ",-";
+});
+
